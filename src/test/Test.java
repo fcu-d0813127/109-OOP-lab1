9b41611b2b1349450b082546b5b7909724db4e91
user <user@EB10>
2020-09-29 09:39:39 +0800

    第一版

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    System.out.println("I'm ªL®m»õ");
+    System.out.println("I'm Jimmy");
     System.out.println("My nid is D0813127");
     System.out.println("This is lab 1 hw");
   }
